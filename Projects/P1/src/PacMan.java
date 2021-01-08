2f7ba242f90a5ff6494111ffed02fa2821a3d516
Lydia Hancock <ljhan@terpmail.umd.edu>
2021-01-08 17:27:51 -0500

    added code for Pacman get_valid_moves function

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -15,7 +15,39 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		ArrayList<Location> listValidMoves = new ArrayList<Location>();
+		int xCoor = myLoc.x;
+		int yCoor = myLoc.y;
+		
+		/* checking type in field - left side of current location */
+		Location locLeft = new Location(xCoor - 1, yCoor);
+		/* move left on map possible */
+		if (!myMap.getLoc(locLeft).contains(Map.Type.WALL)) {
+			listValidMoves.add(locLeft);
+		}
+		
+		/* checking type in field - right side of current location */
+		Location locRight = new Location(xCoor + 1, yCoor);
+		/* move right on map possible */
+		if (!myMap.getLoc(locRight).contains(Map.Type.WALL)) {
+			listValidMoves.add(locRight);
+		}
+		
+		/* checking type in field - top side of current location */
+		Location locAbove  = new Location(xCoor, yCoor - 1);
+		/* move up on map possible */
+		if (!myMap.getLoc(locAbove).contains(Map.Type.WALL)) {
+			listValidMoves.add(locAbove);
+		}
+		
+		/* checking type in field - bottom side of current location */
+		Location locBelow = new Location(xCoor, yCoor + 1);
+		/* move sown on map possible */
+		if (!myMap.getLoc(locBelow).contains(Map.Type.WALL)) {
+			listValidMoves.add(locBelow);
+		}
+		
+		return listValidMoves;	
 	}
 
 	public boolean move() {
