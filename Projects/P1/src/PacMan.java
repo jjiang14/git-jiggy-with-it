0647e5e0db464fb9ef644242acb8920f68dc97e9
Lydia Hancock <ljhan@terpmail.umd.edu>
2021-01-09 16:19:22 -0500

    updated Pacman-get_valid_moves function to ignore Locations with Map.Type.GHOST updated test cases to TestPacManValidMoves

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -16,6 +16,12 @@ public class PacMan{
 		this.myMap = map;
 	}
 
+	/* Returns an arraylist of Locations that represent valid moves 
+	 * that Pacman is able to make given his current location
+	 * If there are no valid moves an empty arraylist is returned. 
+	 * Valid moves include: 
+	 * any Location with Map.Type.COOKIE 
+	 * any location with Map.Type.EMPTY */
 	public ArrayList<Location> get_valid_moves() {
 		ArrayList<Location> listValidMoves = new ArrayList<Location>();
 		int xCoor = myLoc.x;
@@ -25,7 +31,7 @@ public class PacMan{
 		Location locLeft = new Location(xCoor - 1, yCoor);
 		HashSet<Map.Type> hashSet = myMap.getLoc(locLeft);
 		/* move left on map possible */
-		if (hashSet != null && !hashSet.contains(Map.Type.WALL)) {
+		if (hashSet != null && !hashSet.contains(Map.Type.WALL) && !hashSet.contains(Map.Type.GHOST)) {
 			listValidMoves.add(locLeft);
 		}
 		
@@ -33,7 +39,7 @@ public class PacMan{
 		Location locRight = new Location(xCoor + 1, yCoor);
 		hashSet = myMap.getLoc(locRight);
 		/* move right on map possible */
-		if (hashSet != null && !hashSet.contains(Map.Type.WALL)) {
+		if (hashSet != null && !hashSet.contains(Map.Type.WALL) && !hashSet.contains(Map.Type.GHOST)) {
 			listValidMoves.add(locRight);
 		}
 		
@@ -41,7 +47,7 @@ public class PacMan{
 		Location locAbove  = new Location(xCoor, yCoor - 1);
 		hashSet = myMap.getLoc(locAbove);
 		/* move up on map possible */
-		if (hashSet != null && !hashSet.contains(Map.Type.WALL)) {
+		if (hashSet != null && !hashSet.contains(Map.Type.WALL) && !hashSet.contains(Map.Type.GHOST)) {
 			listValidMoves.add(locAbove);
 		}
 		
@@ -49,7 +55,7 @@ public class PacMan{
 		Location locBelow = new Location(xCoor, yCoor + 1);
 		hashSet = myMap.getLoc(locBelow);
 		/* move down on map possible */
-		if (hashSet != null && !hashSet.contains(Map.Type.WALL)) {
+		if (hashSet != null && !hashSet.contains(Map.Type.WALL) && !hashSet.contains(Map.Type.GHOST)) {
 			listValidMoves.add(locBelow);
 		}
 		
