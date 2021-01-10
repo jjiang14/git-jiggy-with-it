35f8e75fdd97f01605b9dcedc8dccd6361521dad
ShaneMehr <shanelock11@gmail.com>
2021-01-10 17:30:53 -0500

    Added tests for map attack

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,9 +1,26 @@
 import java.awt.Color;
 import junit.framework.*;
+import org.junit.*;
 
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		/* creates new map */
+		MainFrame frame = new MainFrame();
+
+		/* Creates a ghost named Gerald that will be adjacent to pacman */
+		Ghost closeGhost = frame.addGhost(new Location(1, 1), "Gerald", Color.red);
+		/* Creates a ghost named Ruford that will not be adjacent to pacman */
+		Ghost farGhost = frame.addGhost(new Location(2, 2), "Ruford", Color.blue);
+
+		/* creates pacman */
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+
+		/* checks that Gerald can successfully attack pacman */
+		assertTrue(frame.getMap().attack("Gerald"));
+		/* checks that Ruford is too far away and cannot attack pacman */
+		assertFalse(frame.getMap().attack("Ruford"));
+		/* checks that a non-existent ghost is unable to attack pacman */
+		assertFalse(frame.getMap().attack("Bobby"));
 	}
 }
