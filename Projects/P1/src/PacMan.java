c5a7b54c64e2007b54631d344618798f47795ee6
ShaneMehr <shanelock11@gmail.com>
2021-01-09 19:04:31 -0500

    Finished the is_ghost_in_range method

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -23,6 +23,16 @@ public class PacMan{
 	}
 
 	public boolean is_ghost_in_range() { 
+		/* iterates through surrounding grid spaces */
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				/* checks if Type GHOST is present at that grid space */
+				if (myMap.getLoc(new Location(myLoc.x + i, myLoc.y + j)).contains(Map.Type.GHOST)) {
+					return true;
+				}
+			}
+		}
+		/* if no Type GHOST is found then return false */
 		return false;
 	}
 
