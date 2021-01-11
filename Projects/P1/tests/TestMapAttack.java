bb9cc978953c33cd9e12057ead4a9e78bf22207d
Shane Mehr <36009767+ShaneMehr@users.noreply.github.com>
2021-01-10 22:06:54 -0500

    Update TestMapAttack.java

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -10,16 +10,12 @@ public class TestMapAttack {
 
 		/* Creates a ghost named Gerald that will be adjacent to pacman */
 		Ghost closeGhost = frame.addGhost(new Location(1, 1), "Gerald", Color.red);
-		/* Creates a ghost named Ruford that will not be adjacent to pacman */
-		Ghost farGhost = frame.addGhost(new Location(2, 2), "Ruford", Color.blue);
 
 		/* creates pacman */
 		PacMan pacman = frame.addPacMan(new Location(0, 0));
 
 		/* checks that Gerald can successfully attack pacman */
 		assertTrue(frame.getMap().attack("Gerald"));
-		/* checks that Ruford is too far away and cannot attack pacman */
-		assertFalse(frame.getMap().attack("Ruford"));
 		/* checks that a non-existent ghost is unable to attack pacman */
 		assertFalse(frame.getMap().attack("Bobby"));
 	}
