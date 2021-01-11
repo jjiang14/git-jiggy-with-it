52b688271bc5a6107a2ab1e0fc52e7ca26d72818
ryan_shriver <zognots@gmail.com>
2021-01-11 18:03:39 -0500

    modified testGhostMove_3ghost1Pacman

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -53,7 +53,6 @@ public class TestGhostMove extends TestCase {
 		/* will be used for setting and adding a ghost to the map */
 		Location locGhost = new Location(1, 1);
 		Ghost ghost_7Walls = new Ghost("ghost", locGhost, map_7Walls);
-		
 
 		for (int row = 0; row < 3; row++) { 
 			for (int col = 0; col < 3; col++) {
@@ -88,38 +87,41 @@ public class TestGhostMove extends TestCase {
 		Map map_3ghost1Pacman = new Map(3);
 		
 		/* will be used for setting and adding a ghost to the map */
-		Location locGhost = new Location(1, 1);
-		Ghost ghost_3ghost1Pacman = new Ghost("ghost", locGhost, map_3ghost1Pacman);
+		Location ghost0Loc = new Location(1, 1);
+		Location ghost1Loc = new Location(1, 0);
+		Location ghost2Loc = new Location(2, 1);
+		Location pacLoc = new Location(1, 1);
+		Ghost ghost0 = new Ghost("ghost0", ghost0Loc, map_3ghost1Pacman);
+		Ghost ghost1 = new Ghost("ghost1", ghost1Loc, map_3ghost1Pacman);
+		Ghost ghost2 = new Ghost("ghost2", ghost2Loc, map_3ghost1Pacman);
+		PacMan pacman = new PacMan("pacman", pacLoc, map_3ghost1Pacman);
 		
 		/* will be used for adding Pacman to the map */
 		Location locPacman = new Location(0, 1);
 
-		for (int row = 0; row < 3; row++) { 
-			for (int col = 0; col < 3; col++) {
-				Location loc = new Location(row, col);
-				
-				/* setting GHOST */
-				if (row == 2 && col == 1) {
-					map_3ghost1Pacman.add("pacman", locPacman, new PacManComponent(row, col, 20), Map.Type.PACMAN);
-					continue;
-				} else if (row == 0) {
-					map_3ghost1Pacman.add("ghost", loc, new GhostComponent(row, col, 20), Map.Type.GHOST);
-					continue;
-				} else if (row == 1 && col == 1) {
-					map_3ghost1Pacman.add("ghost", locGhost, new GhostComponent(row, col, 20), Map.Type.GHOST);
-					continue;
-				}
-				/* using WallComponent because get_valid_moves is setup to check the 
-				 * Map.Type at all surrounding locations of an object */
-				map_3ghost1Pacman.add("empty", loc, new WallComponent(row, col, 20), Map.Type.EMPTY);
-			}
-		}
+		map_3ghost1Pacman.add("pacman", locPacman, new PacManComponent(locPacman.x, locPacman.y, 20), Map.Type.PACMAN);
+		map_3ghost1Pacman.add("ghost0", ghost0Loc, new GhostComponent(ghost0Loc.x, ghost0Loc.y, 20), Map.Type.GHOST);
+		map_3ghost1Pacman.add("ghost1", ghost1Loc, new GhostComponent(ghost1Loc.x, ghost1Loc.y, 20), Map.Type.GHOST);
+		map_3ghost1Pacman.add("ghost2", ghost2Loc, new GhostComponent(ghost2Loc.x, ghost2Loc.y, 20), Map.Type.GHOST);
+		/* using WallComponent because get_valid_moves is setup to check the 
+		* Map.Type at all surrounding locations of an object */
+		map_3ghost1Pacman.add("wall", new Location(0, 0), new WallComponent(0, 0, 20), Map.Type.WALL);
 
-		ArrayList<Location> returnedList = ghost_3ghost1Pacman.get_valid_moves();
-		
-		ghost_3ghost1Pacman.move();
-		assertTrue(ghost_3ghost1Pacman.myLoc.equals(returnedList.get(0)));
-		assertTrue(map_3ghost1Pacman.getLoc(returnedList.get(0)).contains(Map.Type.GHOST));
+		Location expectedLoc0 = ghost0.get_valid_moves().get(0);
+		ghost0.move();
+		Location expectedLoc1 = ghost1.get_valid_moves().get(0);
+		ghost1.move();
+		Location expectedLoc2 = ghost2.get_valid_moves().get(0);
+		ghost2.move();
+		//System.out.println("\nExpected location 0: " + expectedLoc0 + "\n" + ghost0.myLoc + "\n" + map_3ghost1Pacman.getLoc(expectedLoc0) + "\n");
+		assertTrue(ghost0.myLoc.equals(expectedLoc0));
+		assertTrue(map_3ghost1Pacman.getLoc(expectedLoc0).contains(Map.Type.GHOST));
+		//System.out.println("\nExpected location 1: " + expectedLoc1 + "\n" + ghost1.myLoc + "\n" + map_3ghost1Pacman.getLoc(expectedLoc1) + "\n");
+		assertTrue(ghost1.myLoc.equals(expectedLoc1));
+		//assertTrue(map_3ghost1Pacman.getLoc(expectedLoc1).contains(Map.Type.GHOST));
+		//System.out.println("\nExpected location 2: " + expectedLoc2 + "\n" + ghost2.myLoc + "\n" + map_3ghost1Pacman.getLoc(expectedLoc2) + "\n");
+		assertTrue(ghost2.myLoc.equals(expectedLoc2));
+		assertTrue(map_3ghost1Pacman.getLoc(expectedLoc2).contains(Map.Type.GHOST));
 
 		return;
 	}
